053fdbc671589b23f17b159950d63d32c7243a3a
mohitsonu <mohitsonu33@gmail.com>
2025-02-14 03:09:42 +0530

    #

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,7 @@ function TaskList({ tasks, deleteTask, completeTask }) {
   const getPriorityColor = (priority) => {
     switch (priority) {
       case 'High':
-        return 'red';
+        return 'rgb(214, 60, 18)';
       case 'Medium':
         return 'orange';
       case 'Low':
@@ -15,19 +15,24 @@ function TaskList({ tasks, deleteTask, completeTask }) {
   };
 
   return (
-    <ul className="task-list">
-      {tasks.map((task) => (
-        <li key={task.id} className="task-item">
-          <span style={{ color: getPriorityColor(task.priority) }}>
-            {task.title}
-          </span>
-          {!task.completed && (
-            <button className="complete-button" onClick={() => completeTask(task.id)}>Complete</button>
-          )}
-          <button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h2>Tasks to Complete</h2>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={task.id} className="task-item">
+            <span style={{ color: getPriorityColor(task.priority) }}>
+              {index + 1}. {task.title} ({task.priority})
+            </span>
+            <div className="task-buttons">
+              {!task.completed && (
+                <button className="complete-button" onClick={() => completeTask(task.id)}>Complete</button>
+              )}
+              <button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
